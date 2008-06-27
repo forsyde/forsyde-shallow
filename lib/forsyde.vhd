3d30b6276627cec1970c39b6f2909d85ff09ce20
alfonso.acosta <alfonso.acosta@gmail.com>
2008-06-28 01:00:48 +0200

    VHDL Backend: forsyde.vhd: add show function for all default types and redefined int{8,16,32} as subtypes of integer
    darcs-hash:20080627230048-331b3-6c1b97b3e898f117802b01ae0880c161660e3e7f

--- lib/forsyde.vhd
+++ lib/forsyde.vhd
@@ -3,29 +3,28 @@ use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
   
 package types is
-
   
+  function default return integer;
+  function show (i : integer) return string;  
   
 -- Commented out due to representation overflow (modelsim integers
 -- are 32bits long)
---  type int64 is range -(2**(64-1)) to +(2**(64-1)-1);  -- 64 bit integers 
---  function default return int64;
+-- subtype int64 is integer range -(2**(64-1)) to +(2**(64-1)-1);  
 
 -- Commented out due to a overflow in 2**32
---  type int32 is range -(2**(32-1)) to +(2**(32-1)-1);  -- 32 bit integers 
-  type int32 is range -2147483648 to +2147483647;  -- 32 bit integers 
-  function default return int32;
+-- subtype int32 is integer range -(2**(32-1)) to +(2**(32-1)-1);  
+  subtype int32 is integer range -2147483648 to +2147483647;  
+  
+  subtype int16 is integer range -(2**(16-1)) to +(2**(16-1)-1);  
 
-  type int16 is range -(2**(16-1)) to +(2**(16-1)-1);  -- 16 bit integers
-  function default return int16;
-
-  type int8 is range -(2**(8-1)) to +(2**(8-1)-1);   -- 8 bit integers
-  function default return int8;
+  subtype int8 is integer range -(2**(8-1)) to +(2**(8-1)-1);
 
   function default return std_logic;
-
+  function show (s : std_logic) return string;
+  
   function default return boolean;
-
+  function show (b : boolean) return string;
+  
   -- Indexes for unconstrained fsvecs:
   --  -1 is used to express the null vector, with bounds (0 to -1)
   subtype fsvec_index is integer range -1 to integer'high;
@@ -40,31 +39,44 @@ package body types is
 --   return 0;
 --  end default;
 
-  function default return int32 is
+  function default return integer is
   begin 
    return 0;
   end default;
 
-  function default return int16 is
+  function show (i : integer) return string is
   begin
-   return 0;
-  end default;
-
-  
-  function default return int8 is
-  begin
-   return 0;
-  end default;
+    return integer'image(i);
+  end show;
 
   function default return std_logic is
   begin
    return '0';
   end default;
 
+  function show (s : std_logic) return string is
+  begin
+    if s = '1' then
+      return "H";
+    else
+      return "L";
+    end if;
+  end show;
+
+  
   function default return boolean is
   begin
    return true;
   end default;
+
+  function show (b : boolean) return string is
+  begin
+    if b then
+      return "True";
+    else
+      return "False";
+    end if;
+  end show;
 
   
 end types;
