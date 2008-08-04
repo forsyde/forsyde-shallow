84e52b6ba8ae883b1bc161c73ef5776aa60691e8
alfonso.acosta <alfonso.acosta@gmail.com>
2008-08-04 18:48:18 +0200

    Test: finished adding VHDL tests for the examples
    darcs-hash:20080804164818-331b3-f61b1998bb250a3c5094389d5bbe52060bbc5afc

--- tests/properties/VHDLBackend.hs
+++ tests/properties/VHDLBackend.hs
@@ -15,13 +15,24 @@ import ZipTwist
 import CarrySelectAdder     
 import Null
 
+import Control.Monad (liftM, replicateM)
 import Data.List (transpose)
 import Data.Param.FSVec (vectorTH, reallyUnsafeVector)
+import System.Random
 import Test.HUnit
 import ForSyDe
+import Data.Int
 
 vhdlBackendTest :: Test
-vhdlBackendTest = test [aluTest, counterTest]
+vhdlBackendTest = test [aluTest, 
+                        counterTest,
+                        parAddFourTest,
+                        muxTest,
+                        muxFSVecTest,
+                        seqAddFourTest,
+                        buttonEncoderTest,
+                        zipTwistTest,
+                        nullTest]
 
 -- systematic test for the ALU
 aluTest :: Test
@@ -48,21 +59,108 @@ counterTest = "counterTest" ~: "first 400 outputs" ~:
  where out400Sim = take 400 simCounter
        out400VHDLSim = vhdlTest (Just 400) counterSys
 
-vhdlTest :: SysFunToIOSimFun sysF simF => Maybe Int -> SysDef sysF -> simF
-vhdlTest = writeAndModelsimVHDLOps testVHDLOps
+parAddFourTest :: Test
+parAddFourTest = "parAddFourTest" ~: test doTest
+ where -- testt the overflows, and 400 different pseudorandom numbers
+       lastInt32 :: Num a => a
+       lastInt32 = 2147483647
+       firstInt32 :: Num a => a
+       -- FIXME: this should be -2147483648, but due to bug http://code.google.com/p/forsyde-comp/issues/detail?id=22 it was set to -2147483647
+       firstInt32 = -2147483647 
+       gen400Int32 :: IO [Int32]
+       gen400Int32 = do
+          let randomInt32 :: IO Int32
+              randomInt32 = do
+                 let getInt32Range :: IO Integer
+                     getInt32Range = getStdRandom (randomR (firstInt32, lastInt32))
+                 liftM fromIntegral getInt32Range
+          replicateM 400 randomInt32
+       doTest :: IO ()
+       doTest = do  arg1 <- gen400Int32  
+                    arg2 <- gen400Int32
+                    arg3 <- gen400Int32
+                    arg4 <- gen400Int32 
+                    arg5 <- gen400Int32  
+                    let sim = simParAddFour (1:(-1):arg1) 
+                                            -- (firstInt32:lastInt32:arg2)
+                                            -- (lastInt32:firstInt32:arg3)
+                                            arg2
+                                            arg3
+                                            (-1:1:arg4)
+                                            (0:1:arg5)
+                        simVHDL = vhdlTest Nothing
+                                           parAddFourSys
+                                           (1:(-1):arg1) 
+                                           -- (firstInt32:lastInt32:arg2)
+                                           -- (lastInt32:firstInt32:arg3)
+                                           arg2
+                                           arg3
+                                           (-1:1:arg4)
+                                           (0:1:arg5)
+                    resVHDL <- simVHDL
+                    sim @=? resVHDL                    
 
-testVHDLOps :: VHDLOps
-testVHDLOps = defaultVHDLOps{analyzeQuartus=True}
 
-(<~=?>) :: (Eq a, Show a) => a -> IO a -> Test  
-expected <~=?> actualM  = TestCase $ do a <- actualM
-                                        expected @=? a
+muxTest :: Test
+muxTest = "muxTest" ~:  Multiplexer.simMux sel input <~=?> 
+                        vhdlTest Nothing Multiplexer.muxSysDef sel input
+ where sel = [(L,L),(L,H),(H,L),(H,H)]
+       input = [(L,L,L,H),(L,L,H,L),(L,H,L,L),(H,L,L,L)]
+        
 
+
+muxFSVecTest :: Test
+muxFSVecTest = "muxTest" ~:  Multiplexer_FSVector.simMux sel input <~=?> 
+                        vhdlTest Nothing Multiplexer_FSVector.muxSysDef sel input
+ where sel = [$(vectorTH [L,L]),$(vectorTH [L,H]),$(vectorTH [H,L]),$(vectorTH [H,H])]
+       input = [$(vectorTH [L,L,L,H]), $(vectorTH [L,L,H,L]), $(vectorTH [L,H,L,L]),
+                $(vectorTH [H,L,L,L])]
+
+seqAddFourTest :: Test
+seqAddFourTest = "seqAddFourTest" ~: simAddFour input <~=?>
+                                     vhdlTest Nothing addFourSys input
+ where input = [-100,-99..100] 
+
+buttonEncoderTest :: Test
+buttonEncoderTest = "buttonEncoderTest" ~: simButtonEncoder input <~=?>
+                                           vhdlTest Nothing buttonEncoderSys input
+ where input = [(H,L,L,L), (L,H,L,L), (L,L,H,L), (L,L,L,H), (L,L,L,L)]
+
+zipTwistTest :: Test
+zipTwistTest = "zipTwistTest" ~: 
+  simZipTwist input1 input2 input3 input4 input5 input6 <~=?> 
+  vhdlTest Nothing zipTwistSys input1 input2 input3 input4 input5 input6
+ where input1 = [-100..0]
+       input2 = [-10..90]
+       input3 = [100..200]
+       input4 = [350..450]
+       input5 = [-400..(-300)]
+       input6 = [-50..50]
+
+nullTest :: Test
+-- we don't test quartus here, because it complains about no logic
+nullTest = "nullTest" ~: simNull <~=?>  writeAndModelsimVHDL Nothing nullSysDef 
+       
 
 
 -------------------
 -- Helper functions
 -------------------
+
+
+-- run a vhdl testbench with custom backend options
+vhdlTest :: SysFunToIOSimFun sysF simF => Maybe Int -> SysDef sysF -> simF
+vhdlTest = writeAndModelsimVHDLOps testVHDLOps
+
+-- testing VHDL options
+testVHDLOps :: VHDLOps
+testVHDLOps = defaultVHDLOps{analyzeQuartus=True}
+
+-- Compare an IO test with a pure expected result
+(<~=?>) :: (Eq a, Show a) => a -> IO a -> Test  
+expected <~=?> actualM  = TestCase $ do a <- actualM
+                                        expected @=? a
+
 
 -- | permutation a list
 --   Each element of the input list indicates the posibilities of each
