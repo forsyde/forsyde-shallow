cb66df389cf60d7fe166b2faac4879bc0876f4c9
ingo <ingo@kth.se>
2009-10-29 11:51:54 +0100

    AdaptivityLib updated
    Ignore-this: 4db9091ca92f8a0614f0ca5289874170
    
    darcs-hash:20091029105154-ecb6c-1e751cf6ea70cf8f9041d0d8a4dc05eb4036f143

--- src/ForSyDe/Shallow/AdaptivityLib.hs
+++ src/ForSyDe/Shallow/AdaptivityLib.hs
@@ -11,15 +11,23 @@
 -- Adaptivity Library, yet to be completed.
 -- 
 -----------------------------------------------------------------------------
-module ForSyDe.Shallow.AdaptivityLib (applyfSY, applyfU) where
+module ForSyDe.Shallow.AdaptivityLib (applyfSY, applyf2SY, applyf3SY, 
+                                      applyfU) where
 
 import ForSyDe.Shallow.Signal
 import ForSyDe.Shallow.SynchronousLib
 import ForSyDe.Shallow.UntimedLib
 
 applyfSY :: Signal (a -> b) -> Signal a -> Signal b
-applyfSY = zipWithSY apply
-           where apply f x = f x
+applyfSY = zipWithSY ($)
+
+applyf2SY :: Signal (a -> c -> d) 
+          -> Signal a -> Signal c -> Signal d
+applyf2SY = zipWith3SY ($)
+
+applyf3SY :: Signal (a -> c -> d -> e) 
+          -> Signal a -> Signal c -> Signal d -> Signal e
+applyf3SY = zipWith4SY ($)
 
 applyfU :: Int -> Signal ([a] -> [b]) -> Signal a -> Signal b
 applyfU tokenNum = comb2UC tokenNum apply
