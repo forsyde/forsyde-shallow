57b75968598b7266adf618eaef8ac2b457d7ac99
Ingo Sander <ingo@kth.se>
2017-09-30 18:04:22 +0200

    MoCInterfaces added

--- src/ForSyDe/Shallow/DomainInterfaces.hs
+++ src/ForSyDe/Shallow/DomainInterfaces.hs
@@ -12,8 +12,8 @@
 -- model.
 -----------------------------------------------------------------------------
 module ForSyDe.Shallow.DomainInterfaces(downDI, upDI, par2serxDI, ser2parxDI, 
-			par2ser2DI, par2ser3DI, par2ser4DI, 
-			ser2par2DI, ser2par3DI, ser2par4DI) where
+                        par2ser2DI, par2ser3DI, par2ser4DI, 
+                        ser2par2DI, ser2par3DI, ser2par4DI) where
 
 import ForSyDe.Shallow.CoreLib
 import ForSyDe.Shallow.SynchronousLib
@@ -33,7 +33,7 @@ par2ser3DI :: Signal a -> Signal a -> Signal a -> Signal a
 
 -- | The domain interface constructor 'par2ser4DI' converts four parallel signals into one signal
 par2ser4DI :: Signal a -> Signal a -> Signal a -> Signal a 
-	   -> Signal a
+           -> Signal a
 
 
 -- | The domain interface constructor 'par2serxDI' converts n parallel signals into one signal.
@@ -47,34 +47,34 @@ ser2par3DI :: Signal a -> (Signal (AbstExt a), Signal (AbstExt a), Signal (AbstE
 
 -- | The domain interface constructor 'ser2par4DI' converts one signal into four parallel signals.
 ser2par4DI :: Signal a 
-	   -> (Signal (AbstExt a), Signal (AbstExt a), 
+           -> (Signal (AbstExt a), Signal (AbstExt a), 
                Signal (AbstExt a), Signal (AbstExt a))
 
 -- | The domain interface constructors 'ser2parxDI' converts one signal into n parallel signals.
 ser2parxDI :: (Num a, Ord a) => a -> Signal (AbstExt b) 
-				  -> Vector (Signal (AbstExt b))
+                                  -> Vector (Signal (AbstExt b))
 
 -- Implementation
 
 downDI n xs     = down1 n 1 xs 
   where down1 _ _ NullS   = NullS
-	down1 1 1 (x:-xs) = x :- down1 1 1 xs
-	down1 n 1 (x:-xs) = x :- down1 n 2 xs
-	down1 n m (_:-xs) = if m == n then
-			       down1 n 1 xs
-			    else
-			       down1 n (m+1) xs 
+        down1 1 1 (x:-xs) = x :- down1 1 1 xs
+        down1 n 1 (x:-xs) = x :- down1 n 2 xs
+        down1 n m (_:-xs) = if m == n then
+                               down1 n 1 xs
+                            else
+                               down1 n (m+1) xs 
 
 upDI _ NullS   = NullS
 upDI n (x:-xs) = (Prst x) :- ((copyS (n-1) Abst) +-+ upDI n xs)
 
 par2ser2DI xs ys  = par2ser2DI' (zipSY xs ys)
   where par2ser2DI' NullS = NullS
-	par2ser2DI' ((x,y):-xys) = x:-y:-par2ser2DI' xys
+        par2ser2DI' ((x,y):-xys) = x:-y:-par2ser2DI' xys
 
 par2ser3DI xs ys zs = par2ser3DI' (zip3SY xs ys zs)
   where par2ser3DI' NullS = NullS
-	par2ser3DI' ((x,y,z):-xyzs) = x:- y :-z :- par2ser3DI' xyzs
+        par2ser3DI' ((x,y,z):-xyzs) = x:- y :-z :- par2ser3DI' xyzs
 
 par2ser4DI ws xs ys zs = par2ser4DI' (zip4SY ws xs ys zs)
   where par2ser4DI' NullS = NullS
@@ -90,11 +90,11 @@ ser2par4DI = unzip4SY . group4SY . delaynSY Abst 4 . mapSY abstExt
 
 par2serxDI = par2serxDI' . zipxSY 
   where par2serxDI' NullS    = NullS
-	par2serxDI' (xv:-xs) = (signal . fromVector) xv 
-			          +-+ par2serxDI' xs 
+        par2serxDI' (xv:-xs) = (signal . fromVector) xv 
+                                  +-+ par2serxDI' xs 
 
 ser2parxDI n = unzipxSY . delaySY (copyV n Abst) 
-			. filterAbstDI . group n
+                        . filterAbstDI . group n
 
 group2SY :: Signal t -> Signal (t, t)
 group2SY NullS = NullS
@@ -122,9 +122,9 @@ filterAbstDI ((Prst x):-xs) = x :- filterAbstDI xs
 
 group :: (Ord a, Num a) => a -> Signal a1 -> Signal (AbstExt (Vector a1))
 group n xs = mapSY (output n) (scanlSY (addElement n)  (NullV, 0) xs)
-	       where addElement m (vs, n) x | n < m  = (vs <: x, n+1)
-      				            | n == m = (unitV x, 1)
+               where addElement m (vs, n) x | n < m  = (vs <: x, n+1)
+                                            | n == m = (unitV x, 1)
                                             | otherwise = error "Vector of wrong size"
- 		     output m (vs, n) | m == n = Prst vs
-				      | otherwise = Abst
+                     output m (vs, n) | m == n = Prst vs
+                                      | otherwise = Abst
 
--- src/ForSyDe/Shallow/Signal.hs
+++ src/ForSyDe/Shallow/Signal.hs
@@ -12,47 +12,47 @@
 -- functions operating on it.
 -----------------------------------------------------------------------------
 module ForSyDe.Shallow.Signal( Signal (NullS, (:-)), (-:), (+-+), (!-), 
-	       signal, fromSignal,
-	       unitS, nullS, headS, tailS, atS, takeS, dropS,
-	       lengthS, infiniteS, copyS, selectS, writeS, readS, fanS
-	     ) where
+               signal, fromSignal,
+               unitS, nullS, headS, tailS, atS, takeS, dropS,
+               lengthS, infiniteS, copyS, selectS, writeS, readS, fanS
+             ) where
 
-infixr 5	:-
-infixr 5	-:
-infixr 5	+-+
-infixr 5	!-
+infixr 5        :-
+infixr 5        -:
+infixr 5        +-+
+infixr 5        !-
 
 
 -- | A signal is defined as a list of events. An event has a tag and a value. The tag of an event is defined by the position in the list. A signal is defined as an instance of the classes 'Read' and 'Show'. The signal 1 :- 2 :- NullS is represented as \{1,2\}.
 data Signal a = NullS
-	      | a :- Signal a deriving (Eq)
+              | a :- Signal a deriving (Eq)
 
 -- | The function 'signal' converts a list into a signal.
-signal		   :: [a] -> Signal a 
+signal             :: [a] -> Signal a 
 
 -- | The function 'fromSignal' converts a signal into a list.
-fromSignal	   :: Signal a -> [a]
+fromSignal         :: Signal a -> [a]
 
 -- | The function 'unitS' creates a signal with one value.
-unitS		   :: a -> Signal a
+unitS              :: a -> Signal a
 
 -- | The function 'nullS' checks if a signal is empty.
-nullS		   :: Signal a -> Bool
+nullS              :: Signal a -> Bool
 
 -- | The function 'headS' gives the first value - the head -  of a signal.
-headS		   :: Signal a -> a
+headS              :: Signal a -> a
 
 -- | The function 'tailS' gives the rest of the signal - the tail.
-tailS		   :: Signal a -> Signal a
+tailS              :: Signal a -> Signal a
 
 -- | The function 'atS'  returns the n-th event in a signal. The numbering of events in a signal starts with 0. There is also an operator version of this function, '(!-)'.
-atS		   :: Int -> Signal a -> a
+atS                :: Int -> Signal a -> a
 
 -- | The function 'takeS' returns the first n values of a signal.
-takeS		   :: Int -> Signal a -> Signal a
+takeS              :: Int -> Signal a -> Signal a
 
 -- | The function 'dropS' drops the first $n$ values from a signal.
-dropS		   :: Int -> Signal a -> Signal a
+dropS              :: Int -> Signal a -> Signal a
 
 -- | The function 'selectS' takes three parameters, an offset, a stepsize and a signal and returns some elements of the signal such as in the following example:
 --
@@ -60,17 +60,17 @@ dropS		   :: Int -> Signal a -> Signal a
 -- Signal> selectS 2 3 (signal[1,2,3,4,5,6,7,8,9,10])
 -- {3,6,9} :: Signal Integer
 -- @
-selectS		   :: Int -> Int -> Signal a -> Signal a
+selectS                    :: Int -> Int -> Signal a -> Signal a
 
 -- | The function 'lengthS' returns the length of a 'finite' signal.
-lengthS		   :: Signal b -> Int
+lengthS                    :: Signal b -> Int
 
 -- | The function 'infiniteS' creates an infinite signal. The first argument 'f' is a function that is applied on the current value. The second argument 'x' gives the first value of the signal.
 --
 -- > Signal> takeS 5 (infiniteS (*3) 1)
 -- > {1,3,9,27,81} :: Signal Integer
 --
-infiniteS	   :: (a -> a) -> a -> Signal a
+infiniteS          :: (a -> a) -> a -> Signal a
 
 -- | The function 'writeS' transforms a signal into a string of the following format:
 --
@@ -78,7 +78,7 @@ infiniteS	   :: (a -> a) -> a -> Signal a
 -- Signal> writeS (signal[1,2,3,4,5])
 -- "1\n2\n3\n4\n5\n" :: [Char]
 -- @
-writeS		   :: Show a => Signal a -> [Char]
+writeS             :: Show a => Signal a -> [Char]
 
 -- | The function 'readS' transforms a formatted string into a signal.
 --
@@ -86,13 +86,13 @@ writeS		   :: Show a => Signal a -> [Char]
 -- Signal> readS "1\n2\n3\n4\n5\n" :: Signal Int
 -- {1,2,3,4,5} :: Signal Int
 -- @
-readS		   :: Read a => [Char] -> Signal a
+readS              :: Read a => [Char] -> Signal a
 
 -- | The operator '-:' adds at an element to a signal at the tail.
-(-:)		   :: Signal a -> a -> Signal a
+(-:)               :: Signal a -> a -> Signal a
 
 -- | The operator '+-+' concatinates two signals into one signal.  
-(+-+)		   :: Signal a -> Signal a -> Signal a 
+(+-+)              :: Signal a -> Signal a -> Signal a 
 
 
 -- | The function 'copyS' creates a signal with n values 'x'.
@@ -106,79 +106,79 @@ fanS :: (Signal a -> Signal b) -> (Signal a -> Signal c)
 -- Implementation
 
 instance (Show a) => Show (Signal a) where
-	 showsPrec p NullS = showParen (p > 9) (
-	 		       showString "{}")
-	 showsPrec p xs	   = showParen (p > 9) (
-			       showChar '{' . showSignal1 xs)
-			     where
-				showSignal1 NullS
-			           = showChar '}'
-			        showSignal1 (y:-NullS) 
-			           = shows y . showChar '}'
-				showSignal1 (y:-ys)    
-		                   = shows y . showChar ',' 
-				     . showSignal1 ys
+         showsPrec p NullS = showParen (p > 9) (
+                               showString "{}")
+         showsPrec p xs            = showParen (p > 9) (
+                               showChar '{' . showSignal1 xs)
+                             where
+                                showSignal1 NullS
+                                   = showChar '}'
+                                showSignal1 (y:-NullS) 
+                                   = shows y . showChar '}'
+                                showSignal1 (y:-ys)    
+                                   = shows y . showChar ',' 
+                                     . showSignal1 ys
 
 instance Read a => Read (Signal a) where
-	 readsPrec _ s = readsSignal s
+         readsPrec _ s = readsSignal s
 
-readsSignal	       :: (Read a) => ReadS (Signal a)
-readsSignal s	       =  [((x:-NullS), rest) 
-			    | ("{", r2)   <- lex s,
-			      (x, r3)     <- reads r2,
-			      ("}", rest) <- lex r3]
-		          ++ [(NullS, r4)	    
-			    | ("{", r5) <- lex s,
-			      ("}", r4) <- lex r5]
-		          ++ [((x:-xs), r6)	    
-			    | ("{", r7) <- lex s,
-			      (x, r8)	<- reads r7,
-			      (",", r9) <- lex r8,
-			      (xs, r6)	<- readsValues r9]
+readsSignal            :: (Read a) => ReadS (Signal a)
+readsSignal s          =  [((x:-NullS), rest) 
+                            | ("{", r2)   <- lex s,
+                              (x, r3)     <- reads r2,
+                              ("}", rest) <- lex r3]
+                          ++ [(NullS, r4)           
+                            | ("{", r5) <- lex s,
+                              ("}", r4) <- lex r5]
+                          ++ [((x:-xs), r6)         
+                            | ("{", r7) <- lex s,
+                              (x, r8)   <- reads r7,
+                              (",", r9) <- lex r8,
+                              (xs, r6)  <- readsValues r9]
 
-readsValues	       :: (Read a) => ReadS (Signal a)
-readsValues s	       =  [((x:-NullS), r1) 
-		            | (x, r2)   <- reads s,
-			      ("}", r1) <- lex r2]
-		          ++ [((x:-xs), r3)    
-	   		    | (x, r4)   <- reads s,
-			      (",", r5) <- lex r4,
-			      (xs, r3)  <- readsValues r5]
+readsValues            :: (Read a) => ReadS (Signal a)
+readsValues s          =  [((x:-NullS), r1) 
+                            | (x, r2)   <- reads s,
+                              ("}", r1) <- lex r2]
+                          ++ [((x:-xs), r3)    
+                            | (x, r4)   <- reads s,
+                              (",", r5) <- lex r4,
+                              (xs, r3)  <- readsValues r5]
 
-signal []			=  NullS
-signal (x:xs)			=  x :- signal xs 
+signal []                       =  NullS
+signal (x:xs)                   =  x :- signal xs 
 
-fromSignal NullS		=  []
-fromSignal (x:-xs)		=  x : fromSignal xs
+fromSignal NullS                =  []
+fromSignal (x:-xs)              =  x : fromSignal xs
 
-unitS x				=  x :- NullS
+unitS x                                 =  x :- NullS
 
-nullS NullS			=  True
-nullS _				=  False
+nullS NullS                     =  True
+nullS _                                 =  False
 
-headS NullS			=  error "headS : Signal is empty"
-headS (x:-_)			=  x
+headS NullS                     =  error "headS : Signal is empty"
+headS (x:-_)                    =  x
 
-tailS NullS			=  error "tailS : Signal is empty"
-tailS (_:-xs)			=  xs
+tailS NullS                     =  error "tailS : Signal is empty"
+tailS (_:-xs)                   =  xs
 
-atS _ NullS			
+atS _ NullS                     
       =  error "atS: Signal has not enough elements"
-atS 0 (x:-_)			=  x
-atS n (_:-xs)			=  atS (n-1) xs
+atS 0 (x:-_)                    =  x
+atS n (_:-xs)                   =  atS (n-1) xs
 
 (!-) :: Signal a -> Int -> a
-(!-) xs n			= atS n xs
+(!-) xs n                       = atS n xs
 
-takeS 0 _			= NullS
-takeS _ NullS			= NullS
-takeS n (x:-xs) | n <= 0	= NullS
-		| otherwise	= x :- takeS (n-1) xs
+takeS 0 _                       = NullS
+takeS _ NullS                   = NullS
+takeS n (x:-xs) | n <= 0        = NullS
+                | otherwise     = x :- takeS (n-1) xs
 
-dropS 0 NullS			= NullS
-dropS _ NullS			= NullS 
-dropS n (x:-xs) | n <= 0	= x:-xs
-       	  | otherwise		= dropS (n-1) xs
+dropS 0 NullS                   = NullS
+dropS _ NullS                   = NullS 
+dropS n (x:-xs) | n <= 0        = x:-xs
+                  | otherwise           = dropS (n-1) xs
 
 
 selectS offset step xs = select1S step (dropS offset xs) 
@@ -186,20 +186,20 @@ selectS offset step xs = select1S step (dropS offset xs)
    select1S _  NullS   = NullS
    select1S st (y:-ys) = y :- select1S st (dropS (st-1) ys) 
 
-(-:) xs x			= xs +-+ (x :- NullS)
+(-:) xs x                       = xs +-+ (x :- NullS)
 
-(+-+) NullS   ys		= ys
-(+-+) (x:-xs) ys		= x :- (xs +-+ ys)
+(+-+) NullS   ys                = ys
+(+-+) (x:-xs) ys                = x :- (xs +-+ ys)
 
-lengthS NullS			= 0
-lengthS (_:-xs)			= 1 + lengthS xs
+lengthS NullS                   = 0
+lengthS (_:-xs)                         = 1 + lengthS xs
 
-infiniteS f x			= x :- infiniteS f (f x)
+infiniteS f x                   = x :- infiniteS f (f x)
 
-copyS 0 _			= NullS
-copyS n x			= x :- copyS (n-1) x
+copyS 0 _                       = NullS
+copyS n x                       = x :- copyS (n-1) x
 
-fanS p1 p2 xs			= (p1 xs, p2 xs)
+fanS p1 p2 xs                   = (p1 xs, p2 xs)
 
 writeS NullS   = []
 writeS (x:-xs) = show x ++ "\n" ++ writeS xs
@@ -208,7 +208,7 @@ readS xs       = readS' (words xs)
   where
     readS' []        = NullS
     readS' ("\n":ys) = readS' ys
-    readS' (y:ys)	 = read y :- readS' ys
+    readS' (y:ys)        = read y :- readS' ys
 
 
 
