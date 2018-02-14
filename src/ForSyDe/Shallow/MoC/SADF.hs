adba268388676d3e660c1a46128650fcfc011a89
RBonna <ricardobonna@gmail.com>
2018-02-14 19:12:44 +0100

    Added 9 more kernels and the Register Bank example

--- src/ForSyDe/Shallow/MoC/SADF.hs
+++ src/ForSyDe/Shallow/MoC/SADF.hs
@@ -16,21 +16,22 @@ module ForSyDe.Shallow.MoC.SADF (
   -- * Combinational Process Constructors
   -- | Combinational process constructors are used for processes that
   -- do not have a state.
-  mapSADF, zipWithSADF, zipWith3SADF, zipWith4SADF,
+  mapSADF, zipWithSADF, zipWith3SADF, zipWith4SADF, zipWith5SADF,
   -- * Sequential Process Constructors
   -- | Sequential process constructors are used for processes that
   -- have a state. One of the input parameters is the initial state.
   delaySADF, delaynSADF,
   -- * Processes
   -- | Processes to unzip a signal of tupels into a tuple of signals
-  unzipSADF, unzip3SADF, unzip4SADF,
+  unzipSADF, unzip3SADF, unzip4SADF, unzip5SADF,
   -- * Kernels
   -- | Based on the process constructors in the SADF-MoC, the
   -- SADF-library provides SADF-kernels with single or multiple inputs
-  kernel11SADF, kernel12SADF, kernel13SADF, kernel14SADF,
-  kernel21SADF, kernel22SADF, kernel23SADF, kernel24SADF,
-  kernel31SADF, kernel32SADF, kernel33SADF, kernel34SADF,
-  kernel41SADF, kernel42SADF, kernel43SADF, kernel44SADF,
+  kernel11SADF, kernel12SADF, kernel13SADF, kernel14SADF, kernel15SADF,
+  kernel21SADF, kernel22SADF, kernel23SADF, kernel24SADF, kernel25SADF,
+  kernel31SADF, kernel32SADF, kernel33SADF, kernel34SADF, kernel35SADF,
+  kernel41SADF, kernel42SADF, kernel43SADF, kernel44SADF, kernel45SADF,
+  kernel51SADF, kernel52SADF, kernel53SADF, kernel54SADF, kernel55SADF,
   -- * Detectors
   -- | Based on the process constructors in the SADF-MoC, the
   -- SADF-library provides SADF-detectors with single or multiple inputs
@@ -41,7 +42,7 @@ module ForSyDe.Shallow.MoC.SADF (
   ) where
 
 import ForSyDe.Shallow.Core
-import Data.List(unzip4)
+import Data.List(unzip4, unzip5)
 
 ------------------------------------------------------------------------
 -- COMBINATIONAL PROCESS CONSTRUCTORS
@@ -139,6 +140,35 @@ zipWith4SADF ct as bs cs ds
                             consumed_tokens_cs consumed_tokens_ds
 
 
+-- | The process constructor 'zipWith5SADF' takes a signal of scenarios
+-- (tuples with the consumed and produced tokens as well as a function operating
+-- on lists), and results in an SADF-process that takes five input signals and
+-- results in an output signal
+zipWith5SADF :: Signal ((Int, Int, Int, Int, Int), Int, [a] -> [b] -> [c] -> [d] -> [e] -> [f])
+             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e -> Signal f
+zipWith5SADF NullS _ _ _ _ _ = NullS
+zipWith5SADF ct as bs cs ds es
+  | c1 < 0 || c2 < 0 || c3 < 0 || c4 < 0 || c5 < 0
+    = error "zipWith5SADF: Number of consumed tokens must be a non-negative integer"
+  | (not $ sufficient_tokens c1 as)
+    || (not $ sufficient_tokens c2 bs)
+    || (not $ sufficient_tokens c3 cs)
+    || (not $ sufficient_tokens c4 ds)
+    || (not $ sufficient_tokens c5 es) = NullS
+  | otherwise = if length produced_tokens == p then
+                  signal produced_tokens +-+ zipWith5SADF (tailS ct) (dropS c1 as)
+                                              (dropS c2 bs) (dropS c3 cs) (dropS c4 ds) (dropS c5 es)
+                else
+                  error "zipWith5SADF: Function does not produce correct number of tokens"
+  where ((c1, c2, c3, c4, c5), p, f) = headS ct
+        consumed_tokens_as = fromSignal $ takeS c1 as
+        consumed_tokens_bs = fromSignal $ takeS c2 bs
+        consumed_tokens_cs = fromSignal $ takeS c3 cs
+        consumed_tokens_ds = fromSignal $ takeS c4 ds
+        consumed_tokens_es = fromSignal $ takeS c5 es
+        produced_tokens = f consumed_tokens_as consumed_tokens_bs
+                            consumed_tokens_cs consumed_tokens_ds consumed_tokens_es
+
 -------------------------------------
 --             --
 -- SEQUENTIAL PROCESS CONSTRUCTORS --
@@ -199,6 +229,13 @@ kernel41SADF :: Signal ((Int, Int, Int, Int), Int, [a] -> [b] -> [c] -> [d] -> [
              -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e
 kernel41SADF = zipWith4SADF
 
+-- | The process constructor 'kernel51SADF' constructs an SADF kernel with
+-- five data input and one data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel51SADF :: Signal ((Int, Int, Int, Int, Int), Int, [a] -> [b] -> [c] -> [d] -> [e] -> [f])
+             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e -> Signal f
+kernel51SADF = zipWith5SADF
+
 
 -- > Kernals with two outputs
 
@@ -232,6 +269,15 @@ kernel42SADF :: Signal ((Int, Int, Int, Int), (Int, Int), [a] -> [b] -> [c] -> [
              -> (Signal e, Signal f)
 kernel42SADF ct as bs cs ds
   = unzipSADF (get_prodToken ct) $ zipWith4SADF (switch_prodToken ct) as bs cs ds
+
+-- | The process constructor 'kernel52SADF' constructs an SADF kernel with
+-- five data input and two data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel52SADF :: Signal ((Int, Int, Int, Int, Int), (Int, Int), [a] -> [b] -> [c] -> [d] -> [e] -> [([f], [g])])
+             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e
+             -> (Signal f, Signal g)
+kernel52SADF ct as bs cs ds es
+  = unzipSADF (get_prodToken ct) $ zipWith5SADF (switch_prodToken ct) as bs cs ds es
 
 
 -- > Kernals with three outputs
@@ -268,6 +314,16 @@ kernel43SADF :: Signal ((Int, Int, Int, Int), (Int, Int, Int),
 kernel43SADF ct as bs cs ds
   = unzip3SADF (get_prodToken ct) $ zipWith4SADF (switch_prodToken ct) as bs cs ds
 
+-- | The process constructor 'kernel53SADF' constructs an SADF kernel with
+-- five data input and three data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel53SADF :: Signal ((Int, Int, Int, Int, Int), (Int, Int, Int),
+             [a] -> [b] -> [c] -> [d] -> [e] -> [([f], [g], [h])])
+             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e
+             -> (Signal f, Signal g, Signal h)
+kernel53SADF ct as bs cs ds es
+  = unzip3SADF (get_prodToken ct) $ zipWith5SADF (switch_prodToken ct) as bs cs ds es
+
 
 -- > Kernals with four outputs
 
@@ -303,6 +359,63 @@ kernel44SADF :: Signal ((Int, Int, Int, Int), (Int, Int, Int, Int),
              -> (Signal e, Signal f, Signal g, Signal h)
 kernel44SADF ct as bs cs ds
   = unzip4SADF (get_prodToken ct) $ zipWith4SADF (switch_prodToken ct) as bs cs ds
+
+-- | The process constructor 'kernel54SADF' constructs an SADF kernel with
+-- five data input and four data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel54SADF :: Signal ((Int, Int, Int, Int, Int), (Int, Int, Int, Int),
+             [a] -> [b] -> [c] -> [d] -> [e] -> [([f], [g], [h], [i])])
+             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e
+             -> (Signal f, Signal g, Signal h, Signal i)
+kernel54SADF ct as bs cs ds es
+  = unzip4SADF (get_prodToken ct) $ zipWith5SADF (switch_prodToken ct) as bs cs ds es
+
+
+-- > Kernals with five outputs
+
+-- | The process constructor 'kernel15SADF' constructs an SADF kernel with
+-- one data input and five data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel15SADF :: Signal (Int, (Int, Int, Int, Int, Int), [a] -> [([b], [c], [d], [e], [f])])
+             -> Signal a -> (Signal b, Signal c, Signal d, Signal e, Signal f)
+kernel15SADF ct xs = unzip5SADF (get_prodToken ct) $ mapSADF (switch_prodToken ct) xs
+
+-- | The process constructor 'kernel25SADF' constructs an SADF kernel with
+-- two data input and five data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel25SADF :: Signal ((Int, Int), (Int, Int, Int, Int, Int), [a] -> [b] -> [([c], [d], [e], [f], [g])])
+             -> Signal a -> Signal b -> (Signal c, Signal d, Signal e, Signal f, Signal g)
+kernel25SADF ct xs ys = unzip5SADF (get_prodToken ct) $ zipWithSADF (switch_prodToken ct) xs ys
+
+-- | The process constructor 'kernel35SADF' constructs an SADF kernel with
+-- three data input and five data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel35SADF :: Signal ((Int, Int, Int), (Int, Int, Int, Int, Int),
+             [a] -> [b] -> [c] -> [([d], [e], [f], [g], [h])])
+             -> Signal a -> Signal b -> Signal c
+             -> (Signal d, Signal e, Signal f, Signal g, Signal h)
+kernel35SADF ct as bs cs
+  = unzip5SADF (get_prodToken ct) $ zipWith3SADF (switch_prodToken ct) as bs cs
+
+-- | The process constructor 'kernel45SADF' constructs an SADF kernel with
+-- four data input and five data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel45SADF :: Signal ((Int, Int, Int, Int), (Int, Int, Int, Int, Int),
+             [a] -> [b] -> [c] -> [d] -> [([e], [f], [g], [h], [i])])
+             -> Signal a -> Signal b -> Signal c -> Signal d
+             -> (Signal e, Signal f, Signal g, Signal h, Signal i)
+kernel45SADF ct as bs cs ds
+  = unzip5SADF (get_prodToken ct) $ zipWith4SADF (switch_prodToken ct) as bs cs ds
+
+-- | The process constructor 'kernel55SADF' constructs an SADF kernel with
+-- five data input and five data output signals. The scenario (token rates and
+-- function) is determined by the control signal.
+kernel55SADF :: Signal ((Int, Int, Int, Int, Int), (Int, Int, Int, Int, Int),
+             [a] -> [b] -> [c] -> [d] -> [e] -> [([f], [g], [h], [i], [j])])
+             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e
+             -> (Signal f, Signal g, Signal h, Signal i, Signal j)
+kernel55SADF ct as bs cs ds es
+  = unzip5SADF (get_prodToken ct) $ zipWith5SADF (switch_prodToken ct) as bs cs ds es
 
 
 ------------------------------------------------------------------------
@@ -603,6 +716,52 @@ unzip4SADF p xs = (s1, s2, s3, s4)
         else
           error "unzip4SADF: Process does not produce correct number of tokens"
 
+
+unzip5SADF :: [(Int, Int, Int, Int, Int)] -> Signal ([a], [b], [c], [d], [e])
+           -> (Signal a, Signal b, Signal c, Signal d, Signal e)
+unzip5SADF p xs = (s1, s2, s3, s4, s5)
+  where
+    (p1, p2, p3, p4, p5) = unzip5 p
+    s1 = signal $ f1 p1 xs
+    s2 = signal $ f2 p2 xs
+    s3 = signal $ f3 p3 xs
+    s4 = signal $ f4 p4 xs
+    s5 = signal $ f5 p5 xs
+    f1 [] _ = []
+    f1 _ NullS      = []
+    f1 (t:ts) ((as, _, _, _, _):-xs)
+      = if length as == t then
+          as ++ f1 (ts++[t]) xs
+        else
+          error "unzip5SADF: Process does not produce correct number of tokens"
+    f2 [] _ = []
+    f2 _ NullS      = []
+    f2 (t:ts) ((_, bs, _, _, _):-xs)
+      = if length bs == t then
+          bs ++ f2 ts xs
+        else
+          error "unzip5SADF: Process does not produce correct number of tokens"
+    f3 [] _ = []
+    f3 _ NullS      = []
+    f3 (t:ts) ((_, _, cs, _, _):-xs)
+      = if length cs == t then
+          cs ++ f3 ts xs
+        else
+          error "unzip5SADF: Process does not produce correct number of tokens"
+    f4 [] _ = []
+    f4 _ NullS      = []
+    f4 (t:ts) ((_, _, _, ds, _):-xs)
+      = if length ds == t then
+          ds ++ f4 ts xs
+        else
+          error "unzip5SADF: Process does not produce correct number of tokens"
+    f5 [] _ = []
+    f5 _ NullS      = []
+    f5 (t:ts) ((_, _, _, _, es):-xs)
+      = if length es == t then
+          es ++ f5 ts xs
+        else
+          error "unzip5SADF: Process does not produce correct number of tokens"
 ------------------------------------------------------------------------
 --
 -- Helper functions (not exported!)
@@ -750,6 +909,17 @@ outputFSM4 f e = (p, outs)
           where (as, bs, cs, ds) = f x
 
 
+outputFSM5 :: (e -> ([a], [b], [c], [d], [g])) -> Signal e
+           -> ([(Int, Int, Int, Int, Int)] , Signal ([a], [b], [c], [d], [g]))
+outputFSM5 _ NullS = ([], NullS)
+outputFSM5 f e = (p, outs)
+  where outs = f1 e
+        p = f2 e
+        f1 NullS = NullS
+        f1 (x:-xs) = signal [f x] +-+ f1 xs
+        f2 NullS = []
+        f2 (x:-xs) = (length as, length bs, length cs, length ds, length gs) : f2 xs
+          where (as, bs, cs, ds, gs) = f x
 
 ------------------------------------------------------------------------
 --
@@ -777,17 +947,76 @@ y = signal [21 .. 40]
 test1out = test1 ct x y
 
 ---------------------------------------------------------
--- test2: detector22SADF test
+-- test2: Anti Wind-up system
 ---------------------------------------------------------
 
-test2 = detector22SADF (1,2) f g 1
-  where f 1 [a] [b1,b2] = 2
-        f 2 [a] [b1,b2] = 3
-        f 3 [a] [b1,b2] = 1
-        g 1 = ([1], [1,2])
-        g 2 = ([2], [1])
-        g 3 = ([3], [1,2,3])
+-- State transition function for the detector
+f :: (Num a, Ord a) => Int -> [a] -> [a] -> Int
+f 1 [y] [v] = if (y > 100 && v > 0 || y < (-100) && v < 0) then 2 else 1
+f 2 [y] [v] = if (y > 100 && v > 0 || y < (-100) && v < 0) then 2 else 1
 
-test2out = test2 x y
+-- Output function for the detector
+g :: Num a => Int -> ([((Int, Int), Int, [a] -> [a] -> [a])], [(Int, (Int, Int), [a] -> [([a], [a])])])
+g 1 = ([((1,1), 1, \[a] [b] -> [a+b])], [(1, (1,1), \[a] -> [([a], [a])])])
+g 2 = ([((0,1), 1, \_ [b] -> [b])], [(1, (0,1), \[a] -> [([], [a])])])
+
+-- Detector
+detector :: (Num a, Ord a) => Signal a -> Signal a
+         -> (Signal ((Int, Int), Int, [a] -> [a] -> [a]), Signal (Int, (Int, Int), [a] -> [([a], [a])]))
+detector = detector22SADF (1,1) f g 1
+
+syst :: (Num a, Ord a) => Signal a -> Signal a
+syst input = output
+  where output = integrator c1 s1 s3
+        s3 = delaySADF 0 output
+        (s1, s5) = fork c2 input
+        s2 = delaySADF 0 s5
+        (c1, c2) = detector s3 s2
+        fork = kernel12SADF
+        integrator = kernel21SADF
+
+
+---------------------------------------------------------
+-- test3: Register Bank (3 registers)
+---------------------------------------------------------
+
+-- Scenarios list
+scenarios :: Int -> ((Int, Int, Int, Int), (Int, Int, Int, Int),
+             [a] -> [a] -> [a] -> [a] -> [([a], [a], [a], [a])])
+scenarios 0 = ((0,0,0,0), (0,0,0,0), \_ _ _ _ -> [([], [], [], [])])
+scenarios 1 = ((0,1,0,0), (1,1,0,0), \_ [r1] _ _ -> [([r1], [r1], [], [])])
+scenarios 2 = ((0,0,1,0), (1,0,1,0), \_ _ [r2] _ -> [([r2], [], [r2], [])])
+scenarios 3 = ((0,0,0,1), (1,0,0,1), \_ _ _ [r3] -> [([r3], [], [], [r3])])
+scenarios 4 = ((1,1,0,0), (0,1,0,0), \[r1] _ _ _ -> [([], [r1], [], [])])
+scenarios 5 = ((1,0,1,0), (0,0,1,0), \[r2] _ _ _ -> [([], [], [r2], [])])
+scenarios 6 = ((1,0,0,1), (0,0,0,1), \[r3] _ _ _ -> [([], [], [], [r3])])
+scenarios _ = error "scenarios: outside the state list"
+
+switchState :: Int -> [String] -> Int
+switchState _ ["sc0"] = 0     -- No operation (kernel inactive)
+switchState _ ["lr1"] = 1     -- Load r1
+switchState _ ["lr2"] = 2     -- Load r2
+switchState _ ["lr3"] = 3     -- Load r3
+switchState _ ["sr1"] = 4     -- Store r1
+switchState _ ["sr2"] = 5     -- Store r2
+switchState _ ["sr3"] = 6     -- Store r3
+switchState _ _ = error "switchState: Input not recognized"
+
+regDetector = detector11SADF 1 switchState (\e -> [scenarios e]) 0
+regKernel = kernel44SADF
+
+registerBank inputControl inputData = output
+  where ct = regDetector inputControl
+        (output, r1, r2, r3) = regKernel ct inputData r1' r2' r3'
+        r1' = delaySADF 0 r1
+        r2' = delaySADF 0 r2
+        r3' = delaySADF 0 r3
+
+cInput = signal ["lr1","lr2","lr3","sr1","sr2","sr3","sc0","lr1","lr2","lr3","lr1","lr2","lr3"]
+dInput = signal [1..10]
+
+regOutput = registerBank cInput dInput
+-- Expected output {0,0,0,1,2,3,1,2,3}
+
 
 -}
