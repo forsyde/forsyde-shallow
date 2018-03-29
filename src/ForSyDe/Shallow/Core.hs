8aca480a1ded34e84279d73bb3b6b5dde5a1e3fa
José Medeiros <j.edil@ene.unb.br>
2018-03-29 15:15:56 -0300

    Move CT plot functions to Utility.Plot module

--- src/ForSyDe/Shallow/Core.hs
+++ src/ForSyDe/Shallow/Core.hs
@@ -3,22 +3,22 @@
 -- Module  :  ForSyDe
 -- Copyright   :  (c) ForSyDe Group, KTH 2007-2008
 -- License     :  BSD-style (see the file LICENSE)
--- 
+--
 -- Maintainer  :  forsyde@kth.se
 -- Stability   :  experimental
--- Portability :  portable 
--- 
+-- Portability :  portable
+--
 -- The CoreLib is the base for all MoC libraries and is a container
 -- that includes the following libraries:
--- 
+--
 -- * "ForSyDe.Shallow.Core.Signal"
--- 
+--
 -- * "ForSyDe.Shallow.Core.Vector"
--- 
--- * "ForSyDe.Shallow.Core.AbsentExt" 
+--
+-- * "ForSyDe.Shallow.Core.AbsentExt"
 -----------------------------------------------------------------------------
 
-module ForSyDe.Shallow.Core(  
+module ForSyDe.Shallow.Core(
         module ForSyDe.Shallow.Core.Signal,
         module ForSyDe.Shallow.Core.Vector,
         module ForSyDe.Shallow.Core.AbsentExt
@@ -27,11 +27,3 @@ module ForSyDe.Shallow.Core(
 import ForSyDe.Shallow.Core.Vector
 import ForSyDe.Shallow.Core.Signal
 import ForSyDe.Shallow.Core.AbsentExt
-
-
-
-
-
-
-
-
--- src/ForSyDe/Shallow/Utility.hs
+++ src/ForSyDe/Shallow/Utility.hs
@@ -3,7 +3,7 @@
 -- Module      :  ForSyDe.Shallow.Utility
 -- Copyright   :  (c) ForSyDe Group, KTH 2007-2008
 -- License     :  BSD-style (see the file LICENSE)
--- 
+--
 -- Maintainer  :  forsyde-dev@ict.kth.se
 -- Stability   :  experimental
 -- Portability :  portable
@@ -13,22 +13,22 @@
 -- either extend the ForSyDe MoC libraries or add additional
 -- functionality to ForSyDe.
 -----------------------------------------------------------------------------
-module ForSyDe.Shallow.Utility (  
-  module ForSyDe.Shallow.Utility.DFT,    
+module ForSyDe.Shallow.Utility (
+  module ForSyDe.Shallow.Utility.DFT,
   module ForSyDe.Shallow.Utility.Memory,
   module ForSyDe.Shallow.Utility.Queue
+--  module ForSyDe.Shallow.Utility.Plot
   --module ForSyDe.Shallow.Utility.FilterLib,
   --module ForSyDe.Shallow.Utility.Gaussian,
   --module ForSyDe.Shallow.Utility.PolyArith,
   --module ForSyDe.Shallow.MoC.Synchronous.Stochastic
   ) where
 
-import ForSyDe.Shallow.Utility.DFT      
+import ForSyDe.Shallow.Utility.DFT
 import ForSyDe.Shallow.Utility.Memory
 import ForSyDe.Shallow.Utility.Queue
+--import ForSyDe.Shallow.Utility.Plot
 --import ForSyDe.Shallow.Utility.FilterLib
 --import ForSyDe.Shallow.Utility.Gaussian
 --import ForSyDe.Shallow.Utility.PolyArith
 --import ForSyDe.Shallow.MoC.Synchronous.Stochastic
-
-
