0260006771c9424c42cbde7dca3de44c5e0330e5
ugeorge <ugeorge@kth.se>
2019-01-10 23:25:02 +0100

    fixed feedback bug (presumably #21). Needs extensive testing.

--- src/ForSyDe/Shallow/MoC/SDF.hs
+++ src/ForSyDe/Shallow/MoC/SDF.hs
@@ -73,14 +73,14 @@ delaynSDF initial_tokens xs = signal initial_tokens +-+ xs
 -- the process constructor takes the number of consumed and produced
 -- tokens and the function of the actor as arguments.
 actor11SDF :: Int -> Int -> ([a] -> [b]) -> Signal a -> Signal b
-actor11SDF c p f s1 = produceSDF p $ mapSDF c f s1 
+actor11SDF = mapSDF 
 
 -- | The process constructor 'actor21SDF' constructs an SDF actor with
 -- two input and one output signals. For each input or output signal,
 -- the process constructor takes the number of consumed and produced
 -- tokens and the function of the actor as arguments.
 actor21SDF :: (Int, Int) -> Int -> ([a] -> [b] -> [c]) -> Signal a -> Signal b -> Signal c    
-actor21SDF c p f s1 s2 = produceSDF p $ zipWithSDF c f s1 s2
+actor21SDF = zipWithSDF
 
 -- | The process constructor 'actor31SDF' constructs an SDF actor with
 -- three input and one output signals. For each input or output signal,
@@ -88,7 +88,7 @@ actor21SDF c p f s1 s2 = produceSDF p $ zipWithSDF c f s1 s2
 -- tokens and the function of the actor as arguments.
 actor31SDF :: (Int, Int, Int) -> Int -> ([a] -> [b] -> [c] -> [d])
        -> Signal a -> Signal b -> Signal c -> Signal d   
-actor31SDF c p f s1 s2 s3 = produceSDF p $ zipWith3SDF c f s1 s2 s3
+actor31SDF = zipWith3SDF
 
 -- | The process constructor 'actor41SDF' constructs an SDF actor with
 -- four input and one output signals. For each input or output signal,
@@ -97,7 +97,7 @@ actor31SDF c p f s1 s2 s3 = produceSDF p $ zipWith3SDF c f s1 s2 s3
 actor41SDF :: (Int, Int, Int, Int) -> Int 
     -> ([a] -> [b] -> [c] -> [d] -> [e]) 
     -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e 
-actor41SDF c p f s1 s2 s3 s4 = produceSDF p $ zipWith4SDF c f s1 s2 s3 s4
+actor41SDF = zipWith4SDF
 
 
 -- > Actors with two outputs
@@ -108,7 +108,7 @@ actor41SDF c p f s1 s2 s3 s4 = produceSDF p $ zipWith4SDF c f s1 s2 s3 s4
 -- tokens and the function of the actor as arguments.
 actor12SDF :: Int -> (Int, Int) -> ([a] -> ([b], [c]))
            -> Signal a -> (Signal b, Signal c)
-actor12SDF c (p1,p2) f xs = unzipSDF (p1, p2) $ mapSDF c f xs  
+actor12SDF c (p1,p2) f xs = unzipSDF (p1,p2) $ mapSDF c 1 (wrapF1 f) xs  
 
 -- | The process constructor 'actor22SDF' constructs an SDF actor with
 -- two input and two output signals. For each input or output signal,
@@ -116,7 +116,7 @@ actor12SDF c (p1,p2) f xs = unzipSDF (p1, p2) $ mapSDF c f xs
 -- tokens and the function of the actor as arguments.
 actor22SDF :: (Int, Int) -> (Int, Int) -> ([a] -> [b] -> ([c], [d]))
            -> Signal a -> Signal b -> (Signal c, Signal d)
-actor22SDF (c1, c2) (p1, p2) f xs ys = unzipSDF (p1, p2) $ zipWithSDF (c1, c2) f xs ys
+actor22SDF (c1,c2) (p1,p2) f xs ys = unzipSDF (p1,p2) $ zipWithSDF (c1,c2) 1 (wrapF2 f) xs ys
 
 -- | The process constructor 'actor32SDF' constructs an SDF actor with
 -- three input and two output signals. For each input or output signal,
@@ -125,8 +125,8 @@ actor22SDF (c1, c2) (p1, p2) f xs ys = unzipSDF (p1, p2) $ zipWithSDF (c1, c2) f
 actor32SDF :: (Int, Int, Int) -> (Int, Int)
            -> ([a] -> [b] -> [c] -> ([d], [e]))
            -> Signal a -> Signal b -> Signal c -> (Signal d, Signal e)
-actor32SDF (c1, c2, c3) (p1, p2) f as bs cs
-  = unzipSDF (p1, p2) $ zipWith3SDF (c1, c2, c3) f as bs cs
+actor32SDF (c1,c2,c3) (p1,p2) f as bs cs
+  = unzipSDF (p1,p2) $ zipWith3SDF (c1,c2,c3) 1 (wrapF3 f) as bs cs
 
 -- | The process constructor 'actor42SDF' constructs an SDF actor with
 -- four input and two output signals. For each input or output signal,
@@ -136,8 +136,8 @@ actor42SDF :: (Int, Int, Int, Int) -> (Int, Int)
            -> ([a] -> [b] -> [c] -> [d] -> ([e], [f])) 
            -> Signal a -> Signal b -> Signal c -> Signal d 
            -> (Signal e, Signal f)
-actor42SDF (c1, c2, c3, c4) (p1, p2) f as bs cs ds 
-  = unzipSDF (p1, p2)$ zipWith4SDF (c1, c2, c3, c4) f as bs cs ds
+actor42SDF (c1,c2,c3,c4) (p1,p2) f as bs cs ds 
+  = unzipSDF (p1,p2) $ zipWith4SDF (c1,c2,c3,c4) 1 (wrapF4 f) as bs cs ds
 
 -- > Actors with three outputs
 
@@ -148,7 +148,7 @@ actor42SDF (c1, c2, c3, c4) (p1, p2) f as bs cs ds
 actor13SDF :: Int -> (Int, Int, Int) 
            -> ([a] -> ([b], [c], [d])) 
            -> Signal a -> (Signal b, Signal c, Signal d)
-actor13SDF c (p1, p2, p3) f xs = unzip3SDF (p1, p2, p3) $ mapSDF c f xs  
+actor13SDF c (p1,p2,p3) f xs = unzip3SDF (p1,p2,p3) $ mapSDF c 1 (wrapF1 f) xs  
 
 -- | The process constructor 'actor23SDF' constructs an SDF actor with
 -- two input and three output signals. For each input or output signal,
@@ -158,8 +158,8 @@ actor23SDF :: (Int, Int) -> (Int, Int, Int)
            -> ([a] -> [b] -> ([c], [d], [e])) 
            -> Signal a -> Signal b 
            -> (Signal c, Signal d, Signal e)
-actor23SDF (c1, c2) (p1, p2, p3) f xs ys
-  = unzip3SDF (p1, p2, p3) $ zipWithSDF (c1, c2) f xs ys
+actor23SDF (c1,c2) (p1,p2,p3) f xs ys
+  = unzip3SDF (p1,p2,p3) $ zipWithSDF (c1,c2) 1 (wrapF2 f) xs ys
 
 -- | The process constructor 'actor33SDF' constructs an SDF actor with
 -- three input and three output signals. For each input or output signal,
@@ -168,8 +168,8 @@ actor23SDF (c1, c2) (p1, p2, p3) f xs ys
 actor33SDF :: (Int, Int, Int) -> (Int, Int, Int) 
            -> ([a] -> [b] -> [c] -> ([d], [e], [f])) 
            -> Signal a -> Signal b -> Signal c -> (Signal d, Signal e, Signal f)
-actor33SDF (c1, c2, c3) (p1, p2, p3) f as bs cs
-  = unzip3SDF (p1, p2, p3) $ zipWith3SDF (c1, c2, c3) f as bs cs
+actor33SDF (c1,c2,c3) (p1,p2,p3) f as bs cs
+  = unzip3SDF (p1,p2,p3) $ zipWith3SDF (c1,c2,c3) 1 (wrapF3 f) as bs cs
 
 -- | The process constructor 'actor43SDF' constructs an SDF actor with
 -- four input and three output signals. For each input or output signal,
@@ -179,8 +179,8 @@ actor43SDF :: (Int, Int, Int, Int) -> (Int, Int, Int)
            -> ([a] -> [b] -> [c] -> [d] -> ([e], [f], [g])) 
            -> Signal a -> Signal b -> Signal c -> Signal d 
            -> (Signal e, Signal f, Signal g)
-actor43SDF (c1, c2, c3, c4) (p1, p2, p3) f as bs cs ds 
-  = unzip3SDF (p1, p2, p3)$ zipWith4SDF (c1, c2, c3, c4) f as bs cs ds
+actor43SDF (c1,c2,c3,c4) (p1,p2,p3) f as bs cs ds 
+  = unzip3SDF (p1,p2,p3)$ zipWith4SDF (c1,c2,c3,c4) 1 (wrapF4 f) as bs cs ds
 
 -- > Actors with four outputs
 
@@ -191,7 +191,7 @@ actor43SDF (c1, c2, c3, c4) (p1, p2, p3) f as bs cs ds
 actor14SDF :: Int -> (Int, Int, Int, Int) 
            -> ([a] -> ([b], [c], [d], [e])) 
            -> Signal a -> (Signal b, Signal c, Signal d, Signal e)
-actor14SDF c (p1, p2, p3, p4) f xs = unzip4SDF (p1, p2, p3, p4) $ mapSDF c f xs  
+actor14SDF c (p1,p2,p3,p4) f xs = unzip4SDF (p1,p2,p3,p4) $ mapSDF c 1 (wrapF1 f) xs  
 
 -- | The process constructor 'actor24SDF' constructs an SDF actor with
 -- two input and four output signals. For each input or output signal,
@@ -201,8 +201,8 @@ actor24SDF :: (Int, Int) -> (Int, Int, Int, Int)
        -> ([a] -> [b] -> ([c], [d], [e], [f])) 
        -> Signal a -> Signal b 
        -> (Signal c, Signal d, Signal e, Signal f)
-actor24SDF (c1, c2) (p1, p2, p3, p4) f xs ys
-  = unzip4SDF (p1, p2, p3, p4) $ zipWithSDF (c1, c2) f xs ys
+actor24SDF (c1,c2) (p1,p2,p3,p4) f xs ys
+  = unzip4SDF (p1,p2,p3,p4) $ zipWithSDF (c1,c2) 1 (wrapF2 f) xs ys
 
 -- | The process constructor 'actor34SDF' constructs an SDF actor with
 -- three input and four output signals. For each input or output signal,
@@ -212,8 +212,8 @@ actor34SDF :: (Int, Int, Int) -> (Int, Int, Int, Int)
            -> ([a] -> [b] -> [c] -> ([d], [e], [f], [g])) 
            -> Signal a -> Signal b -> Signal c
            -> (Signal d, Signal e, Signal f, Signal g)
-actor34SDF (c1, c2, c3) (p1, p2, p3, p4) f as bs cs 
-  = unzip4SDF (p1, p2, p3, p4) $ zipWith3SDF (c1, c2, c3) f as bs cs
+actor34SDF (c1,c2,c3) (p1,p2,p3,p4) f as bs cs 
+  = unzip4SDF (p1,p2,p3,p4) $ zipWith3SDF (c1,c2,c3) 1 (wrapF3 f) as bs cs
 
 -- | The process constructor 'actor14SDF' constructs an SDF actor with
 -- four input and four output signals. For each input or output signal,
@@ -223,8 +223,8 @@ actor44SDF :: (Int, Int, Int, Int) -> (Int, Int, Int, Int)
            -> ([a] -> [b] -> [c] -> [d] -> ([e], [f], [g], [h])) 
            -> Signal a -> Signal b -> Signal c -> Signal d 
            -> (Signal e, Signal f, Signal g, Signal h)
-actor44SDF (c1, c2, c3, c4) (p1, p2, p3, p4) f as bs cs ds 
-  = unzip4SDF (p1, p2, p3, p4)$ zipWith4SDF (c1, c2, c3, c4) f as bs cs ds
+actor44SDF (c1,c2,c3,c4) (p1,p2,p3,p4) f as bs cs ds 
+  = unzip4SDF (p1,p2,p3,p4) $ zipWith4SDF (c1,c2,c3,c4) 1 (wrapF4 f) as bs cs ds
 
 ---------------------------------------------------------------------
 -- COMBINATIONAL PROCESS CONSTRUCTORS
@@ -234,12 +234,15 @@ actor44SDF (c1, c2, c3, c4) (p1, p2, p3, p4) f as bs cs ds
 -- (@c@) and produced (@p@) tokens and a function @f@ that operates on
 -- a list, and results in an SDF-process that takes an input signal
 -- and results in an output signal
-mapSDF :: Int -> ([a] -> b) -> Signal a -> Signal b
-mapSDF _ _ NullS   = NullS
-mapSDF c f xs     
+mapSDF :: Int -> Int -> ([a] -> [b]) -> Signal a -> Signal b
+mapSDF _ _ _ NullS   = NullS
+mapSDF c p f xs     
   | c <= 0 = error "mapSDF: Number of consumed tokens must be positive integer" 
   | not $ sufficient_tokens c xs  = NullS
-  | otherwise  = produced_tokens :- mapSDF c f (dropS c xs) 
+  | otherwise  = if length produced_tokens == p then
+                   signal produced_tokens +-+ mapSDF c p f (dropS c xs) 
+                 else   
+                   error "mapSDF: Function does not produce correct number of tokens" 
   where consumed_tokens = fromSignal $ takeS c xs
         produced_tokens = f consumed_tokens
 
@@ -248,15 +251,18 @@ mapSDF c f xs
 -- the number of produced tokens and a function @f@
 -- that operates on two lists, and results in an SDF-process that takes two
 -- input signals and results in an output signal
-zipWithSDF :: (Int, Int) -> ([a] -> [b] -> c)
+zipWithSDF :: (Int, Int) -> Int -> ([a] -> [b] -> [c])
            -> Signal a -> Signal b -> Signal c                  
-zipWithSDF (_, _) _ NullS _ = NullS
-zipWithSDF (_, _) _ _ NullS = NullS
-zipWithSDF (c1, c2) f as bs 
+zipWithSDF (_, _) _ _ NullS _ = NullS
+zipWithSDF (_, _) _ _ _ NullS = NullS
+zipWithSDF (c1, c2) p f as bs 
   | c1 <= 0 || c2 <= 0  = error "zipWithSDF: Number of consumed tokens must be positive integer"
   | (not $ sufficient_tokens c1 as) 
     || (not $ sufficient_tokens c2 bs) = NullS
-  | otherwise = produced_tokens :- zipWithSDF (c1, c2) f (dropS c1 as) (dropS c2 bs)  
+  | otherwise = if length produced_tokens == p then
+                  signal produced_tokens +-+ zipWithSDF (c1, c2) p f (dropS c1 as) (dropS c2 bs)  
+                else
+                  error "zipWithSDF: Function does not produce correct number of tokens"
   where consumed_tokens_as = fromSignal $ takeS c1 as
         consumed_tokens_bs = fromSignal $ takeS c2 bs
         produced_tokens = f consumed_tokens_as consumed_tokens_bs
@@ -266,12 +272,12 @@ zipWithSDF (c1, c2) f as bs
 -- the number of produced tokens and a function @f@
 -- that operates on three lists, and results in an SDF-process that takes three
 -- input signals and results in an output signal  
-zipWith3SDF :: (Int, Int, Int) -> ([a] -> [b] -> [c] -> d) 
+zipWith3SDF :: (Int, Int, Int) -> Int -> ([a] -> [b] -> [c] -> [d]) 
             -> Signal a -> Signal b -> Signal c -> Signal d                 
-zipWith3SDF (_, _, _) _ NullS _ _= NullS
-zipWith3SDF (_, _, _) _ _ NullS _= NullS
-zipWith3SDF (_, _, _) _ _ _ NullS= NullS
-zipWith3SDF (c1, c2, c3) f as bs cs
+zipWith3SDF (_, _, _) _ _ NullS _ _= NullS
+zipWith3SDF (_, _, _) _ _ _ NullS _= NullS
+zipWith3SDF (_, _, _) _ _ _ _ NullS= NullS
+zipWith3SDF (c1, c2, c3) p f as bs cs
   | c1 <= 0 || c2 <= 0 || c3 <= 0
   = error "zipWith3SDF: Number of consumed tokens must be positive integer"
   | (not $ sufficient_tokens c1 as) 
@@ -279,26 +285,30 @@ zipWith3SDF (c1, c2, c3) f as bs cs
     || (not $ sufficient_tokens c3 cs)
   = NullS
   | otherwise
-  = produced_tokens :- zipWith3SDF (c1, c2, c3) f
-                       (dropS c1 as) (dropS c2 bs) (dropS c3 cs)
+  = if length produced_tokens == p then
+      signal produced_tokens +-+ zipWith3SDF (c1, c2, c3) p f
+                                 (dropS c1 as) (dropS c2 bs) (dropS c3 cs)
+    else
+      error "zipWith3SDF: Function does not produce correct number of tokens"
   where consumed_tokens_as = fromSignal $ takeS c1 as
         consumed_tokens_bs = fromSignal $ takeS c2 bs
         consumed_tokens_cs = fromSignal $ takeS c3 cs
         produced_tokens = f consumed_tokens_as consumed_tokens_bs consumed_tokens_cs
   
+
 -- | The process constructor 'zipWith4SDF' takes a tuple @(c1, c2, c3,c4)@
 -- denoting the number of consumed tokens and an integer @p@
 -- denoting the number of produced tokens and a function @f@ that
 -- operates on three lists, and results in an SDF-process that takes
 -- three input signals and results in an output signal
-zipWith4SDF :: (Int, Int, Int, Int) 
-            -> ([a] -> [b] -> [c] -> [d] -> e) 
+zipWith4SDF :: (Int, Int, Int, Int) -> Int 
+            -> ([a] -> [b] -> [c] -> [d] -> [e]) 
             -> Signal a -> Signal b -> Signal c -> Signal d -> Signal e 
-zipWith4SDF (_, _, _, _) _ NullS _ _ _ = NullS
-zipWith4SDF (_, _, _, _) _ _ NullS _ _ = NullS
-zipWith4SDF (_, _, _, _) _ _ _ NullS _ = NullS
-zipWith4SDF (_, _, _, _) _ _ _ _ NullS = NullS
-zipWith4SDF (c1, c2, c3, c4) f as bs cs ds
+zipWith4SDF (_, _, _, _) _ _ NullS _ _ _ = NullS
+zipWith4SDF (_, _, _, _) _ _ _ NullS _ _ = NullS
+zipWith4SDF (_, _, _, _) _ _ _ _ NullS _ = NullS
+zipWith4SDF (_, _, _, _) _ _ _ _ _ NullS = NullS
+zipWith4SDF (c1, c2, c3, c4) p f as bs cs ds
   | c1 <= 0 || c2 <= 0 || c3 <= 0 || c4 <= 0
   = error "zipWith4SDF: Number of consumed tokens must be positive integer"
   | (not $ sufficient_tokens c1 as) 
@@ -307,24 +317,21 @@ zipWith4SDF (c1, c2, c3, c4) f as bs cs ds
     || (not $ sufficient_tokens c4 ds)    
   = NullS
   | otherwise    
-  = produced_tokens :- zipWith4SDF (c1, c2, c3, c4) f
-           (dropS c1 as) (dropS c2 bs) (dropS c3 cs) (dropS c4 ds)
+  = if length produced_tokens == p then
+      signal produced_tokens +-+ zipWith4SDF (c1, c2, c3, c4) p f
+             (dropS c1 as) (dropS c2 bs) (dropS c3 cs) (dropS c4 ds)
+    else
+      error "zipWith4SDF: Function does not produce correct number of tokens"
   where consumed_tokens_as = fromSignal $ takeS c1 as
         consumed_tokens_bs = fromSignal $ takeS c2 bs
         consumed_tokens_cs = fromSignal $ takeS c3 cs
         consumed_tokens_ds = fromSignal $ takeS c4 ds
         produced_tokens = f consumed_tokens_as consumed_tokens_bs
                             consumed_tokens_cs consumed_tokens_ds
-        
+                            
 ---------------------------------------------------------------------
 -- unzipSDF Processes
 ---------------------------------------------------------------------
-
-produceSDF :: Int -> Signal [a] -> Signal a
-produceSDF p s
-  | allS (\partition -> length partition == p) s
-  = foldrS (\part sig -> signal part +-+ sig) NullS s
-  | otherwise = error "SDF: Function does not produce correct number of tokens"
 
 unzipSDF :: (Int, Int) -> Signal ([a], [b]) 
          -> (Signal a, Signal b)
@@ -334,16 +341,12 @@ unzipSDF (p1, p2) xs = (s1, s2)
     s2 = signal $ f2 xs
     f1 NullS     = []
     f1 ((as, _):-xs)
-      = if length as == p1 then 
-          as ++ f1 xs
-        else  
-          error "unzipSDF: Process does not produce correct number of tokens"
+      | length as == p1 = as ++ f1 xs
+      | otherwise = error "unzipSDF: Process does not produce correct number of tokens"
     f2 NullS     = []
     f2 ((_, bs):-xs)
-      = if length bs == p2 then 
-          bs ++ f2 xs
-        else  
-          error "unzipSDF: Process does not produce correct number of tokens"  
+      | length bs == p2 = bs ++ f2 xs
+      | otherwise = error "unzipSDF: Process does not produce correct number of tokens"  
 
 
 unzip3SDF :: (Int, Int, Int) -> Signal ([a], [b], [c]) 
@@ -355,22 +358,16 @@ unzip3SDF (p1, p2, p3) xs = (s1, s2, s3)
     s3 = signal $ f3 xs
     f1 NullS      = []
     f1 ((as, _, _):-xs) 
-      = if length as == p1 then
-          as ++ f1 xs
-        else  
-          error "unzip3SDF: Process does not produce correct number of tokens"
+      | length as == p1 = as ++ f1 xs
+      | otherwise = error "unzip3SDF: Process does not produce correct number of tokens"
     f2 NullS      = []
     f2 ((_, bs, _):-xs) 
-      = if length bs == p2 then 
-          bs ++ f2 xs
-        else  
-          error "unzip3SDF: Process does not produce correct number of tokens"  
+      | length bs == p2 = bs ++ f2 xs
+      | otherwise = error "unzip3SDF: Process does not produce correct number of tokens"  
     f3 NullS      = []
     f3 ((_, _, cs):-xs) 
-      = if length cs == p3 then 
-          cs ++ f3 xs
-        else  
-          error "unzip3SDF: Process does not produce correct number of tokens"  
+      | length cs == p3 = cs ++ f3 xs
+      | otherwise = error "unzip3SDF: Process does not produce correct number of tokens"  
   
 
 unzip4SDF :: (Int, Int, Int, Int) -> Signal ([a], [b], [c], [d]) 
@@ -383,28 +380,20 @@ unzip4SDF (p1, p2, p3, p4) xs = (s1, s2, s3, s4)
     s4 = signal $ f4 xs
     f1 NullS      = []
     f1 ((as, _, _, _):-xs) 
-      = if length as == p1 then
-          as ++ f1 xs
-        else  
-          error "unzip4SDF: Process does not produce correct number of tokens"
+      | length as == p1 = as ++ f1 xs
+      | otherwise = error "unzip4SDF: Process does not produce correct number of tokens"
     f2 NullS      = []
     f2 ((_, bs, _, _):-xs) 
-      = if length bs == p2 then 
-          bs ++ f2 xs
-        else  
-          error "unzip4SDF: Process does not produce correct number of tokens"  
+      | length bs == p2 = bs ++ f2 xs
+      | otherwise = error "unzip4SDF: Process does not produce correct number of tokens"  
     f3 NullS      = []
     f3 ((_, _, cs, _):-xs) 
-      = if length cs == p3 then 
-          cs ++ f3 xs
-        else  
-          error "unzip4SDF: Process does not produce correct number of tokens" 
+      | length cs == p3 = cs ++ f3 xs
+      | otherwise = error "unzip4SDF: Process does not produce correct number of tokens" 
     f4 NullS      = []
     f4 ((_, _, _, ds):-xs) 
-      = if length ds == p4 then 
-          ds ++ f4 xs
-        else  
-          error "unzip4SDF: Process does not produce correct number of tokens" 
+      | length ds == p4 = ds ++ f4 xs
+      | otherwise = error "unzip4SDF: Process does not produce correct number of tokens" 
 
 ------------------------------------------------------------------------
 --
@@ -412,14 +401,31 @@ unzip4SDF (p1, p2, p3, p4) xs = (s1, s2, s3, s4)
 --
 ------------------------------------------------------------------------
 
+
 sufficient_tokens :: (Num a, Eq a, Ord a) => a -> Signal t -> Bool
 sufficient_tokens 0 _     = True
 sufficient_tokens _ NullS = False
 sufficient_tokens n (_:-xs)
-  = if n < 0 then
-      error "sufficient_tokens: n must not be negative"
-    else
-      sufficient_tokens (n-1) xs
+ = if n < 0 then
+     error "sufficient_tokens: n must not be negative"
+   else
+     sufficient_tokens (n-1) xs
+
+-- outputTokens :: [(a, b, c)] -> [b]
+-- outputTokens [] = []
+-- outputTokens ((_, b, _):xs) = b : outputTokens xs
+
+wrapF1 :: ([a] -> y) -> [a] -> [y]
+wrapF1 f = (\a -> [f a])
+
+wrapF2 :: ([a] -> [b] -> y) -> [a] -> [b] -> [y]
+wrapF2 f = (\a b -> [f a b])
+
+wrapF3 :: ([a] -> [b] -> [c] -> y) -> [a] -> [b] -> [c] -> [y]
+wrapF3 f = (\a b c -> [f a b c])
+
+wrapF4 :: ([a] -> [b] -> [c] -> [d] -> y) -> [a] -> [b] -> [c] -> [d] -> [y]
+wrapF4 f = (\a b c d -> [f a b c d])
 
 ------------------------------------------------------------------------
 --
