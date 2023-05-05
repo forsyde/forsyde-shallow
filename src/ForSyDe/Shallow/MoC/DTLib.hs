8452ad42ae042e6909fea1657c57d5eeecee236b
Ingo Sander <ingo@kth.se>
2023-05-05 14:21:36 +0200

    Updates DTLib

--- src/ForSyDe/Shallow/MoC/DTLib.hs
+++ src/ForSyDe/Shallow/MoC/DTLib.hs
@@ -21,14 +21,15 @@ module ForSyDe.Shallow.MoC.DTLib (
   -- * Sequential Process Constructors
   -- | Sequential process constructors are used for processes that
   --   have a state. One of the input parameters is the initial state.
-  delayDT, scanldDT, mooreDT, mealyDT,
+  delayDT, scanldDT, scanld2DT, mooreDT, moore2DT, mealyDT, mealy2DT,
   -- * Processes
   -- | Processes to unzip a signal of tuples into a tuple of signals
   zipDT, zip3DT, unzipDT, unzip3DT
   ) where  
 
 import ForSyDe.Shallow.Core
-import ForSyDe.Shallow.MoC.Synchronous( mapSY, zipWithSY, zipSY)
+import ForSyDe.Shallow.MoC.Synchronous( mapSY, zipWithSY, zipWith3SY, zipSY)
+import Data.List (sort, nub)
 
 ------------------------------------------------------------------------
 --
@@ -68,9 +69,19 @@ zipWithDT f xs ys = out
          yValues = getValues timeTags ys
          timeTags = getTimeTags2 xs ys
 
+
 zipWith3DT :: (AbstExt a -> AbstExt b -> AbstExt c -> AbstExt d)
-           -> DTSignal a -> DTSignal b -> DTSignal c -> DTSignal d
-zipWith3DT = undefined
+           -> DTSignal a -> DTSignal b -> DTSignal c
+           -> DTSignal d 
+zipWith3DT f xs ys zs = out 
+   where out = zipSY newTimeTags outValues
+         newTimeTags = timeTags         
+         outValues = zipWith3SY f xValues yValues zValues
+         xValues = getValues timeTags xs
+         yValues = getValues timeTags ys
+         zValues = getValues timeTags zs
+         timeTags = getTimeTags3 xs ys zs
+
 
 ------------------------------------------------------------------------
 --                                 
@@ -100,6 +111,16 @@ scanldDT delay f init input = state
   where state = delayDT delay init nextstate
         nextstate = zipWithDT f input state
 
+scanld2DT :: DiscreteTime
+          -> (AbstExt a -> AbstExt b -> AbstExt c -> AbstExt c)
+          -> AbstExt c
+          -> DTSignal a
+          -> DTSignal b
+          -> DTSignal c
+scanld2DT delay f init xs ys = state
+  where state = delayDT delay init nextstate
+        nextstate = zipWith3DT f xs ys state
+
 mooreDT :: DiscreteTime
         -> (AbstExt a -> AbstExt b -> AbstExt b)
         -> (AbstExt b -> AbstExt c)
@@ -117,9 +138,12 @@ moore2DT :: DiscreteTime
          -> AbstExt c
          -> DTSignal a
          -> DTSignal b
-         -> DTSignal c
          -> DTSignal d
-moore2DT = undefined
+moore2DT delay f g init xs ys = output
+  where state = delayDT delay init nextstate
+        nextstate = zipWith3DT f xs ys state
+        output = mapDT g state
+
         
 mealyDT :: DiscreteTime
         -> (AbstExt a -> AbstExt b -> AbstExt b)
@@ -132,15 +156,33 @@ mealyDT delay f g init input = output
         nextstate = zipWithDT f input state
         output = zipWithDT g input state
 
+mealy2DT :: DiscreteTime
+        -> (AbstExt a -> AbstExt b -> AbstExt c -> AbstExt c)
+        -> (AbstExt a -> AbstExt b -> AbstExt c -> AbstExt d)
+        -> AbstExt c
+        -> DTSignal a
+        -> DTSignal b
+        -> DTSignal d
+mealy2DT delay f g init xs ys = output
+  where state = delayDT delay init nextstate
+        nextstate = zipWith3DT f xs ys state
+        output = zipWith3DT g xs ys state
+
 ------------------------------------------------------------------------
 --
 -- ZIP, UNZIP PROCESSES
 --
 ------------------------------------------------------------------------
 
-zipDT :: DTSignal a -> DTSignal b -> DTSignal (a, b)
-zipDT = undefined
-
+zipDT :: DTSignal a -> DTSignal b -> Signal (DiscreteTime, (AbstExt a, AbstExt b))
+zipDT xs ys = out 
+   where out = zipSY newTimeTags outValues
+         newTimeTags = timeTags         
+         outValues = zipWithSY (,) xValues yValues
+         xValues = getValues timeTags xs
+         yValues = getValues timeTags ys
+         timeTags = getTimeTags2 xs ys
+         
 zip3DT :: DTSignal a -> DTSignal b -> DTSignal c
        -> DTSignal (a, b, c)
 zip3DT = undefined
@@ -163,12 +205,14 @@ getTag (tag, _) = tag
 getValue :: (DiscreteTime, a) -> a
 getValue (_, value) = value
 
+
 getTimeTags :: DTSignal a -> Signal DiscreteTime
 getTimeTags NullS   = NullS
 getTimeTags (x:-xs) = if fst x /= 0 then
                         0 :- fst x :- mapSY fst xs
                       else
                         0 :- mapSY fst xs
+
 
 getValues :: Signal DiscreteTime -> Signal (DiscreteTime, AbstExt a) -> Signal (AbstExt a)
 getValues = getValues' Abst
@@ -185,8 +229,12 @@ getValues' init NullS   NullS
 
 getTimeTags2 :: DTSignal a -> DTSignal b -> Signal DiscreteTime
 getTimeTags2 NullS   NullS   = NullS
-getTimeTags2 NullS   ys      = getTimeTags2' NullS ys
-getTimeTags2 xs       NullS  = getTimeTags2' xs NullS
+getTimeTags2 (x:-xs) NullS
+  | fst x == 0 = getTimeTags2' (x:-xs) NullS
+  | otherwise  = 0 :- getTimeTags2' (x:-xs) NullS
+getTimeTags2 NullS   (y:-ys)
+  | fst y == 0 = getTimeTags2' NullS (y:-ys)
+  | otherwise  = 0 :- getTimeTags2' NullS (y:-ys)
 getTimeTags2 (x:-xs) (y:-ys)
   | fst x == 0 || fst y == 0 = getTimeTags2' (x:-xs) (y:-ys)
   | otherwise                = 0 :- getTimeTags2' (x:-xs) (y:-ys)
@@ -199,6 +247,51 @@ getTimeTags2' (x:-xs) (y:-ys)
   | otherwise            = getTag y :- getTimeTags2' (x:-xs) ys
 getTimeTags2' NullS   ys      = mapSY getTag ys
 getTimeTags2' xs      NullS   = mapSY getTag xs
+
+getTimeTags3 :: DTSignal a -> DTSignal b -> DTSignal c
+             -> Signal DiscreteTime
+getTimeTags3 NullS   NullS   NullS = NullS
+getTimeTags3 (x:-xs) NullS   NullS
+  | fst x == 0 = getTimeTags3' (x:-xs) NullS NullS
+  | otherwise  = 0 :- getTimeTags3' (x:-xs) NullS NullS
+getTimeTags3 NullS   (y:-ys) NullS
+  | fst y == 0 = getTimeTags3' NullS (y:-ys) NullS
+  | otherwise  = 0 :- getTimeTags3' NullS (y:-ys) NullS  
+getTimeTags3 NullS   NullS   (z:-zs)
+  | fst z == 0 = getTimeTags3' NullS NullS (z:-zs)
+  | otherwise  = 0 :- getTimeTags3' NullS NullS (z:-zs)
+getTimeTags3 (x:-xs) (y:-ys) (z:-zs)
+  | fst x == 0 || fst y == 0 || fst z == 0 = getTimeTags3' (x:-xs) (y:-ys) (z:-zs)
+  | otherwise                              = 0 :- getTimeTags3' (x:-xs) (y:-ys) (z:-zs)
+
+getTimeTags3' :: DTSignal a -> DTSignal b -> DTSignal c
+              -> Signal DiscreteTime
+getTimeTags3' = undefined
+{-((xt,xv):-xs) ((yt,yv):-ys) ((zt,zv):-zs)
+  | minimum [xt,yt,zt] == xt
+    | xt == yt && 
+getTimeTags3' xs      NullS  NullS  = mapSY getTag xs
+getTimeTags3' NullS   ys     NullS = mapSY getTag ys
+-}
+
+  
+{- IDEA DOES NOT WORK due to need of access elements in advance!
+getTimeTags :: DTSignal a -> Signal DiscreteTime
+getTimeTags xs = (signal . nub) xs'
+  where xs' = 0 : (fromSignal . mapSY getTag) xs
+
+getTimeTags2 :: DTSignal a -> DTSignal b -> Signal DiscreteTime
+getTimeTags2 xs ys = (signal . nub . sort) (xs' ++ ys')
+   where xs' = 0 : (fromSignal . mapSY getTag) xs
+         ys' = (fromSignal . mapSY getTag) ys
+
+getTimeTags3 ::  DTSignal a -> DTSignal b -> DTSignal c
+             -> Signal DiscreteTime
+getTimeTags3 xs ys zs = (signal . nub . sort . concat) [xs', ys', zs']
+   where xs' = 0 : (fromSignal . mapSY getTag) xs
+         ys' = (fromSignal . mapSY getTag) ys
+         zs' = (fromSignal . mapSY getTag) zs
+-}
 
 liftDT :: (t -> a) -> AbstExt t -> AbstExt a
 liftDT f Abst    = Abst
@@ -237,3 +330,20 @@ s4 = signal [(0, Prst 1)]
 
 s5 :: DTSignal Integer
 s5 = scanldDT 1 (lift2DT (+)) (Prst 0) s4
+
+s6 :: DTSignal Integer
+s6 = mapDT (liftDT (+1)) s1
+
+s7 :: DTSignal Integer
+s7 = zipWithDT (lift2DT (+)) s1 s3
+
+s8 :: DTSignal Integer
+s8 = zipWith3DT (lift3DT add3) s1 s3 s4
+  where add3 x y z = x + y + z
+
+s9 :: DTSignal Integer
+s9 = delayDT 3 Abst s1
+
+s10 :: DTSignal Integer
+s10 = scanld2DT 2 (lift3DT add3) Abst s1 s3
+  where add3 x y z = x + y + z
